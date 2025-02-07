ac6e799b0c57a6c87252854d4cc15cb7c3cdf74f
yuhaochen <cyh278889789@gmail.com>
2025-02-07 10:53:25 +0800

    add more apis

--- packages/pkg-backend/src/index.ts
+++ packages/pkg-backend/src/index.ts
@@ -1,8 +1,47 @@
 import { Elysia } from "elysia";
 import {log} from "pkg-common/src/log";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  // Basic GET endpoint
+  .get("/", () => "Hello Elysia")
+  
+  // GET endpoint with path parameter
+  .get("/users/:id", ({ params: { id } }) => {
+    return { userId: id, message: `Fetching user with id ${id}` };
+  })
+  
+  // POST endpoint for creating resources
+  .post("/users", ({ body }) => {
+    return { 
+      message: "User created",
+      data: body 
+    };
+  })
+  
+  // PUT endpoint for updating resources
+  .put("/users/:id", ({ params: { id }, body }) => {
+    return { 
+      message: `User ${id} updated`,
+      data: body 
+    };
+  })
+  
+  // DELETE endpoint
+  .delete("/users/:id", ({ params: { id } }) => {
+    return { 
+      message: `User ${id} deleted` 
+    };
+  })
+  
+  // GET endpoint with query parameters
+  .get("/search", ({ query }) => {
+    return {
+      message: "Search results",
+      query: query
+    };
+  })
 
+  .listen(3000);
 log.info(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
